8f80813df4d3e52549c9d1822e56a62992e1ca51
Chris Graham <chisnet@hotmail.com>
2015-02-27 17:25:17 +0000

    Hacky, but fully working example completed

--- js/smarticle.js
+++ js/smarticle.js
@@ -1,20 +1,57 @@
 /* Smarticle v0.01 */
 (function($){
-    $.fn.smarticle = function(options) {
+    $.fn.smarticle = function(smartifacts, options) {
         var elements = this;
         var $moving_element;
+        var id_counter = 0;
+        var preview_timeout = 0;
+
+        function get_id() {
+            id_counter += 1;
+            return 'smartifact_field_' + id_counter;
+        }
+
+        function slugify(str) {
+            var sane_str = str.replace(/[^A-Za-z0-9\s]/g, '').replace(/\s/g,'_');
+            sane_str = sane_str.replace(/^_+/g, '').replace(/_+$/g, '').replace(/_+/g, '_');
+            return sane_str.toLowerCase();
+        }
+
+        function render_preview() {
+            console.clear();
+            var $preview_area = $('#preview_area');
+            var output = '';
+            var $smartifacts = $('.smarticle-layout .smartifact');
+            for(var i=0; i<$smartifacts.length; i++){
+                var $smartifact = $($smartifacts[i]);
+                var smartifact_type = $smartifact.data('type');
+                var $fields = $smartifact.find('input, textarea, select');
+                var field_data = {};
+                for(var j=0; j<$fields.length; j++){
+                    var $field = $($fields[j]);
+                    var field_name = $field.data('name');
+                    field_data[field_name] = $field.val();
+                }
+                //var output = smartifacts[smartifact_type]['parse']
+                output += smartifacts[smartifact_type]['parse'](field_data);
+            }
+            console.log(output);
+            $preview_area.html(output);
+        }
 
         if(elements.length > 0) {
             var opts = $.extend({}, $.fn.smarticle.defaults, options);
             var $placeholder = $('<div class="smartifact-wrapper"><div class="smartifact-placeholder"></div></div>');
 
             var dragstart_func = function(e){
-                $moving_element = $(this).parent();
-                e.originalEvent.dataTransfer.setData('origin', 'existing');
-                $(this).css('opacity', '0.5');
-                $(this).parent().hide();
-                $placeholder.find('.smartifact-placeholder').css('height', $(this).innerHeight());
-                $(this).append($placeholder);
+                if(e.originalEvent.explicitOriginalTarget.localName == 'h2' || e.originalEvent.explicitOriginalTarget.parentElement.localName == 'h2') { 
+                    $moving_element = $(this).parent();
+                    e.originalEvent.dataTransfer.setData('origin', 'existing');
+                    $(this).css('opacity', '0.5');
+                    $(this).parent().hide();
+                    $placeholder.find('.smartifact-placeholder').css('height', $(this).innerHeight());
+                    $(this).append($placeholder);
+                }
             };
 
             var dragend_func = function(){
@@ -65,25 +102,51 @@
 
                     if(smartifactOrigin == 'new') {
                         var smartifactType = e.originalEvent.dataTransfer.getData('smartifact');
-                        var smartifactContent = 'Unknown';
-                        switch(smartifactType) {
-                            case 'header':
-                                smartifactContent = 'Header';
-                                break;
-                            case 'text':
-                                smartifactContent = 'Text';
-                                break;
-                            case 'quote':
-                                smartifactContent = 'Quote';
-                                break;
-                            case 'youtube':
-                                smartifactContent = 'YouTube Video';
-                                break;
+                        var smartifactContent = '';
+                        
+                        var smartifactInfo = smartifacts[smartifactType];
+                        var fields = smartifactInfo['fields'];
+
+                        smartifactContent += '<h2>' + smartifactInfo['name'] + '</h2>';
+                        for(var i=0; i<fields.length; i++) {
+                            var field = fields[i];
+                            var field_output = '<div class="field ' + field['type'] + '_field">';
+                            var field_id = get_id();
+                            switch(field['type']){
+                                case 'text':
+                                    field_output += '<label for="' + field_id + '">' + field['name'] + '</label>';
+                                    field_output += '<input id="' + field_id + '" type="text" data-name="' + slugify(field['name']) + '" data-type="' + field['type'] + '"/>';
+                                    break;
+                                case 'markdown':
+                                    field_output += '<label for="' + field_id + '">' + field['name'] + '</label>';
+                                    field_output += '<textarea id="' + field_id + '" data-name="' + slugify(field['name']) + '" data-type="' + field['type'] + '"></textarea>';
+                                    field_output += '<div class="help">Markdown syntax is supported!</div>';
+                                    break;
+                                case 'dropdown':
+                                    field_output += '<label for="' + field_id + '">' + field['name'] + '</label>';
+                                    field_output += '<select id="' + field_id + '" data-name="' + slugify(field['name']) + '" data-type="' + field['type'] + '">';
+                                    for(var j=0; j<field['options'].length; j++) {
+                                        var option = field['options'][j];
+                                        field_output += '<option value="' + slugify(option) + '">' + option + '</option>';
+                                    }
+                                    field_output += '</select>';
+                                    break;
+                            }
+                            field_output += '</div>';
+                            smartifactContent += field_output;
                         }
-                        var $newSmartifact = $('<div class="smartifact-wrapper"><div class="smartifact">' + smartifactContent + '</div></div>');
+
+                        var $newSmartifact = $('<div class="smartifact-wrapper"><div class="smartifact" data-type="' + slugify(smartifactInfo['name']) + '">' + smartifactContent + '</div></div>');
                         $newSmartifact.data('smartifact', smartifactType);
                         $newSmartifact.find('.smartifact').attr('draggable','true').on('dragstart', dragstart_func).on('dragend', dragend_func);
+                        
+                        $newSmartifact.find('input, select, textarea').bind('change', function(){
+                            render_preview();
+                        });
+
                         $placeholder.replaceWith($newSmartifact);
+
+                        render_preview();
                     }
                     else if(smartifactOrigin == 'existing') {
                         $placeholder.replaceWith($moving_element);
